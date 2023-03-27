cfc6cf22cbb1218e9cd4fe7e05acaf4d68e5e385
DenisaCG <denisa.checiu@gmail.com>
2023-03-27 22:00:37 +0200

    show Web Bluetoth API error

--- src/widget.ts
+++ src/widget.ts
@@ -232,6 +232,7 @@ export class LegoBoostModel extends DOMWidgetModel {
 
 export class LegoBoostView extends DOMWidgetView {
   txt_connected: HTMLDivElement;
+  txt_bluetooth: HTMLDivElement;
 
   txt_pitch: HTMLDivElement;
   txt_roll: HTMLDivElement;
@@ -249,8 +250,25 @@ export class LegoBoostView extends DOMWidgetView {
   meter_distance: HTMLMeterElement;
   color_color: HTMLDivElement;
 
+  isWebBluetoothSupported : boolean =  navigator.bluetooth ? true : false;
+
   render() {
     this.el.classList.add('custom-widget');
+
+    // checking if Web Bluetooth API is supported
+    if (!this.isWebBluetoothSupported) {
+
+      // bluetooth error box
+      const bluetooth_box = document.createElement('div');
+      bluetooth_box.classList.add('error-box');
+      this.el.appendChild(bluetooth_box);
+
+      this.txt_bluetooth = document.createElement('div');
+      this.txt_bluetooth.textContent = "Your device doesn't support Web Bluetooth API. Try to turn on Experimental Platform Features from Chrome, by accessing the following link and turning it on: chrome://flags/#enable-experimental-web-platform-features";
+      bluetooth_box.appendChild(this.txt_bluetooth);
+
+      console.log( "Your device doesn't support Web Bluetooth API. Try to turn on Experimental Platform Features from Chrome, by accessing the following link and turning it on: chrome://flags/#enable-experimental-web-platform-features");
+    }
 
     // connection box
     const connection_box = document.createElement('div');
